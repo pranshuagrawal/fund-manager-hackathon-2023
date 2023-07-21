5b118a3992692faf9c8f58e8546b5e0640aa7599
Pranshu Agrawal <pranshuagrawal@Pranshus-Mac-mini.local>
2023-07-21 16:14:06 +0530

    Adding manage expense

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-
+import { useState } from "react";
 import CategoryWise from "./components/CategoryWise";
 import TimeWise from "./components/TimeWise";
 import Metrics from "./components/Metrics";
@@ -7,14 +7,91 @@ import ManageCategories from "./components/ManageCategories";
 
 import { categoryWiseData, monthWiseData, limits, dailyData } from "./data";
 
+import {
+  addCategory,
+  fetchCategories,
+  addTransactions,
+  fetchTransactions,
+} from "./db";
+
 function App() {
-  // https://dribbble.com/shots/11278226-Analytic-dashboard-concept/attachments/2886599?mode=media
+  const [categories, setCategories] = useState({
+    data: [],
+    error: null,
+    loading: false,
+  });
+  const [transactions, setTransactions] = useState({
+    data: [],
+    error: null,
+    loading: false,
+  });
+
+  const addCategoryFn = (obj) => {
+    addCategory(obj).then(fetchCategoriesFn);
+  };
+
+  const fetchCategoriesFn = () => {
+    setCategories({
+      loading: true,
+      data: [],
+      error: null,
+    });
+
+    fetchCategories()
+      .then((categories) => {
+        setCategories({
+          loading: false,
+          data: categories,
+          error: null,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        setCategories((d) => ({
+          loading: false,
+          data: [],
+          error: "Unable to fetch categories",
+        }));
+      });
+  };
+
+  const addTransactionsFn = (obj) => {
+    addTransactions(obj).then(fetchTransactionsFn);
+  };
+
+  const fetchTransactionsFn = () => {
+    setTransactions({
+      loading: true,
+      data: [],
+      error: null,
+    });
+
+    fetchTransactions()
+      .then((transactions) => {
+        setTransactions({
+          loading: false,
+          data: transactions,
+          error: null,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        setTransactions((d) => ({
+          loading: false,
+          data: [],
+          error: "Unable to fetch transactions",
+        }));
+      });
+  };
+
   return (
     <div className="container">
-      {/* <button onClick={handleToggleDrawer}>Toggle Drawer</button> */}
       <div className="container-cta">
         <button className="link">Add Expense</button>
-        <ManageCategories />
+        <ManageCategories
+          addCategoryFn={addCategoryFn}
+          categories={categories}
+        />
       </div>
       <div className="container-inner">
         <Metrics data={categoryWiseData} limits={limits} />
--- src/components/ManageCategories/index.js
+++ src/components/ManageCategories/index.js
@@ -3,12 +3,30 @@ import { AddIcon } from "../../icons/icons";
 import Drawer from "../Drawer";
 import "./style.css";
 
-const ManageCategories = () => {
+const ManageCategories = ({ addCategoryFn, categories }) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const [isAddCategoryOpen, setIsAddCategoryOpen] = useState(false);
 
   const handleToggleDrawer = () => {
     setIsDrawerOpen((prev) => !prev);
+  };
+  const DEF_CAT_STATE = {
+    name: "",
+    type: "expenditure",
+    limit: 10000,
+  };
+  const [addCategoryForm, setAddCategoryForm] = useState({ ...DEF_CAT_STATE });
+
+  const inputHandler = (val, key) => {
+    setAddCategoryForm((d) => ({
+      ...d,
+      [key]: val,
+    }));
+  };
+
+  const addCategoryHandler = () => {
+    addCategoryFn({ ...addCategoryForm });
+    setAddCategoryForm(DEF_CAT_STATE);
   };
 
   return (
@@ -32,19 +50,33 @@ const ManageCategories = () => {
           {isAddCategoryOpen && (
             <div className="add-category">
               <div className="input-label">Category Name</div>
-              <input />
+              <input
+                value={addCategoryForm.name}
+                onChange={(e) => inputHandler(e.target.value, "name")}
+              />
 
               <div className="input-label mt15">Type</div>
-              <select>
-                <option value="Value1">Value 1</option>
+              <select
+                value={addCategoryForm.type}
+                onChange={(e) => inputHandler(e.target.value, "type")}
+              >
+                <option value="expenditure">Expenditure</option>
+                <option value="investment">Investment</option>
               </select>
 
               <div className="input-label  mt15">Category Limit</div>
-              <input />
+              <input
+                value={addCategoryForm.limit}
+                onChange={(e) => inputHandler(e.target.value, "limit")}
+              />
 
-              <button className="primary mt15">Add Category</button>
+              <button className="primary mt15" onClick={addCategoryHandler}>
+                Add Category
+              </button>
             </div>
           )}
+
+          {JSON.stringify(categories)}
         </div>
       </Drawer>
     </>
