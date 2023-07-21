9d94bd6a1f7ff4a28c5178508c57b7d8cd70973a
Pranshu Agrawal <pranshuagrawal@Pranshus-Mac-mini.local>
2023-07-21 10:40:46 +0530

    Graph added

--- src/components/DailyWise/index.jsx
+++ src/components/DailyWise/index.jsx
@@ -1,26 +1,35 @@
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
 const DailyWise = () => {
 
-    const options = {
-  title: {
-    text: 'My chart'
-  },
-  series: [{
-    data: [1, 2, 3]
-  }]
-}
+  const options = {
+    title: {
+      text: null
+    },
+    series: [{
+      data: [1, 2, 3]
+    }],
+    credits: {
+      enabled: false
+    }
+  }
 
-return (
-        <div className="row">
-            <div className="column">
-                <div className="heading">Daily Expenditure</div>
-                <div className="graph-container">Graph</div>
-            </div>
-            <div className="column">
-                <div className="heading">Daily Investment</div>
-                <div className="graph-container">Graph</div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="row">
+      <div className="column">
+        <div className="heading">Daily Expenditure</div>
+        <div className="graph-container"><HighchartsReact
+          highcharts={Highcharts}
+          options={options}
+        /></div>
+      </div>
+      <div className="column">
+        <div className="heading">Daily Investment</div>
+        <div className="graph-container">Graph</div>
+      </div>
+    </div>
+  )
 }
 
 export default DailyWise;
